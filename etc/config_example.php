7e962ae61d26e13fcef709be195d16a17f350b0a
Mik Stavast <44979700+theproatgaming13@users.noreply.github.com>
2026-03-10 09:49:23 +0100

    ee

--- etc/config_example.php
+++ etc/config_example.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL server.
 Please change this file to config.php and change these defines: */
 define('DB_SERVER', 'localhost:3306');
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_password');
-define('DB_NAME', 'db_name');
+define('DB_USERNAME', 'mstavast_mik');
+define('DB_PASSWORD', 'NIBgerNIBger268!@#');
+define('DB_NAME', 'mstavast_database');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
